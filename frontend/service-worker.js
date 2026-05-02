cfdfcf28422c503965edc011b7d9e00fce8fd8a3
gtorrespro <g@gabrieltorrespro.com>
2026-05-02 18:01:01 +0100

    Improve new task modal readability

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260502v4';
+const CACHE_VERSION = '20260502v5';
 const STATIC_CACHE = `zelo-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `zelo-runtime-${CACHE_VERSION}`;
 
